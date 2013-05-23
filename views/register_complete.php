eed85e93aad83fafd2451d3704e0f3567a859d73
Bryun Sakaye <sakaye87@gmail.com>
2013-05-23 11:06:33 -0700

    latest version

--- views/register_complete.php
+++
@@ -1,7 +0,0 @@
-<div class="container">
-		<div class="content_background">
-			<h1 class="underlined">Your registration is almost complete!</h1>
-			<p>Please check your email that you used during registration to complete the process.</p>
-		</div>
-		<div class="clear"></div>		
-<!-- end .container--></div>
